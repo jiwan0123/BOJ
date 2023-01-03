d76b9b61a04407e024674e45799f6781ec3fb8e5
JIWAN KIM <109573350+jiwan0123@users.noreply.github.com>
2023-01-04 03:57:45 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.rs
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main() {
     let stdout = io::stdout();
     let mut out = io::BufWriter::new(stdout.lock());
-    while (true) {
+    loop {
         let mut a = String::new();
         io::stdin().read_line(&mut a).unwrap();
         let num: Vec<i32> = a
@@ -12,10 +12,9 @@ fn main() {
             .map(|a| a.parse().unwrap())
             .collect();
         let result = num[0] + num[1];
-        if result == 0 {
-            break;
-        } else {
-            writeln!(out, "{}", result).unwrap();
+        match result == 0 {
+            true => break,
+            false => writeln!(out, "{}", result).unwrap(),
         }
     }
 }
